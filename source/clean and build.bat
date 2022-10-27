2f067374f15cdd5a3ffc2f9a3e5d26660146fbb9
Ronald Cuello <ronald.cuello@softwareone.com>
2022-10-27 17:37:04 -0500

    added clean and build.bat

---
+++ source/clean and build.bat
@@ -0,0 +1,15 @@
+@echo off
+cd "elibraryConsoleApp\"
+
+
+set JAVA_HOME=C:\Program Files\Java\jdk-18.0.2
+set Maven_Home="C:\Program Files\NetBeans-14\netbeans\java\maven\bin\mvn"
+
+echo Cambiando entorno java a la version %JAVA_HOME%
+
+echo Compilar proyecto
+
+%Maven_Home% clean install
+
+
+pause
